5cd3b7dd3eff6b087d01fe9a21b836ff54845bf2
Hamad1233901 <hamadkhan123801@gmail.com>
2025-12-24 19:38:09 +0000

    Code updated

--- Pharmacis-master/models/Product.php
+++ Pharmacis-master/models/Product.php
@@ -1,6 +1,6 @@
 <?php
 // Filename: models/Product.php
-// Yeh file Products ke CRUD operations (Create, Read, Delete) handle karti hai.
+
 
 class Product {
     private $conn;
@@ -16,7 +16,7 @@ class Product {
         $this->conn = $db;
     }
 
-    // READ (Saray products lana)
+    
     public function read() {
         $query = "SELECT * FROM " . $this->table_name . " ORDER BY id DESC";
         $stmt = $this->conn->prepare($query);
@@ -24,12 +24,12 @@ class Product {
         return $stmt;
     }
 
-    // CREATE (Naya product add karna)
+    
     public function create() {
         $query = "INSERT INTO " . $this->table_name . " SET name=:name, category=:category, price=:price, stock_quantity=:stock";
         $stmt = $this->conn->prepare($query);
 
-        // Sanitize
+        
         $this->name = htmlspecialchars(strip_tags($this->name));
         $this->category = htmlspecialchars(strip_tags($this->category));
         $this->price = htmlspecialchars(strip_tags($this->price));
@@ -43,7 +43,7 @@ class Product {
         return $stmt->execute();
     }
 
-    // DELETE (Product remove karna)
+    
     public function delete() {
         $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
         $stmt = $this->conn->prepare($query);
